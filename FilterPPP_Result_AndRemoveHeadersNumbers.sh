c8a46c4a8e13ae2c5b0b2eb02947c23f7b11a651
mjbieren <107464589+mjbieren@users.noreply.github.com>
2025-07-13 17:58:17 +0200

    Update FilterPPP_Result_AndRemoveHeadersNumbers.sh

--- FilterPPP_Result_AndRemoveHeadersNumbers.sh
+++ FilterPPP_Result_AndRemoveHeadersNumbers.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-PROGRAMPATH=~/Programs/FilterPPPResult/FilterPPPResult_Debian.out #FilterPPPResult_Debian.out Change the path
+PROGRAMPATH=FilterPPPResult_Static_Debian.out #FilterPPPResult_Static_Debian.out Change the path
 INPUT= #<OutputFolderScript13A/phylopypruner_output/output_alignments/ #phylopypruner_output/output_alignments/ #Change This
 
 OUTPUT= #Change This Output Path. Be sure to make it different than Script 14B
-TAXONOMIC_GROUPFILE=~/TaxonomicGroup/Coleocheatephyceae_TaxonomicGroupFile_FilterPPP_Set.txt #Taxonomic Group file see (See /10_OrthogroupSequenceGrabber_OSG/TaxonomicGroupFiles/ for examples)
-SUMMARY_FILE= #Output folder where the summary file ends up. Don't make it the same as the Output folder, since it's hard to find it back otherwise due to the file size.
-NUMBER_OF_FILTER_GROUPS=10 #Change This. Threshold of for the amount of taxonomic groups
+TAXONOMIC_GROUPFILE=~/TaxonomicGroup/Coleocheatephyceae_TaxonomicGroupFile_FilterPPP_Set.txt #Taxonomic Group file (See TaxonomicGroupFiles/ for examples)
+SUMMARY_FILE= #Output folder where the summary file ends up. Please don't make it the same as the Output folder, as it's difficult to find it again otherwise, due to the number of files.
+NUMBER_OF_FILTER_GROUPS=10 #Change This. The threshold for the number of taxonomic groups
 
 #Filter PPP result based on NUMBER_OF_FILTER_GROUPS
 #with gene ids
